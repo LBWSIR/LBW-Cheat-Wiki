1b9b2bc152f36aacb496493d46eb306a5a3de92b
LBWSIR <2134123qq@gmail.com>
2026-05-26 01:52:21 +0800

    feat: 增加教程图片点击放大功能

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,22 @@
+﻿import DefaultTheme from 'vitepress/theme'
+import mediumZoom from 'medium-zoom'
+import { onMounted, watch, nextTick } from 'vue'
+import { useData } from 'vitepress'
+import './custom.css'
+
+export default {
+  extends: DefaultTheme,
+  setup() {
+    const { route } = useData()
+    const initZoom = () => {
+      mediumZoom('.vp-doc img:not(.no-zoom)', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  }
+}
