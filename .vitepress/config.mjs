bd5585664590d325d1a69f17deae4b8a93f320de
LBWSIR <147610576+LBWSIR@users.noreply.github.com>
2026-05-27 04:37:24 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,4 +1,4 @@
-﻿// .vitepress/config.mjs
+// .vitepress/config.mjs
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
@@ -31,7 +31,6 @@ export default defineConfig({
         position: relative;
         display: inline-block;
         -webkit-user-drag: none !important; /* 禁止拖拽图片 */
-        pointer-events: none !important;    /* 禁止右键点击保存图片 */
       }
 
       /* 利用背景图片实现噪点干扰 (Base64 格式免去文件路径烦恼) */
